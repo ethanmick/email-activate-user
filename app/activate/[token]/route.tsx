b7b9bfccb83d9f79b3c42bdc1976c5d5bd7a64f2
Ethan Mick <ethan@ethanmick.com>
2023-06-07 16:06:50 -0400

    fix: Add token in activate route

--- app/activate/[token]/route.tsx
+++ app/activate/[token]/route.tsx
@@ -25,6 +25,9 @@ export async function GET(
                 gt: new Date(Date.now() - 24 * 60 * 60 * 1000), // 24 hours ago
               },
             },
+            {
+              token,
+            },
           ],
         },
       },
